15d4a454946aa5c6a5b3d6d5af6cfd3a89241613
wujq4github <wujq4java@gmail.com>
2017-01-18 11:27:56 +0800

    second commit.

--- src/main/java/com/jin/jdk8/Test1.java
+++ src/main/java/com/jin/jdk8/Test1.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
- * Created by wujq on 2017/1/17.
+ *
  */
 public class Test1 {
     public static void main(String[] args) {
--- src/main/java/com/jin/jdk8/lesson01/Tes.java
+++
@@ -1,9 +0,0 @@
-package com.jin.jdk8.lesson01;
-
-/**
- * 【作者】wujq
- * 【日期】2017/1/17
- *
- */
-public class Tes {
-}
