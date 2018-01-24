5523d8f1c2d43ecd05ad9375332f364e9f9599f7
wujinqing <wujinqing@jetair.com.cn>
2018-01-24 18:03:01 +0800

    sec

---
+++ src/main/java/com/jin/jdk8/defaultmethod/B.java
@@ -0,0 +1,12 @@
+package com.jin.jdk8.defaultmethod;
+
+/**
+ * @author wu.jinqing
+ * @date 2018年01月24日
+ */
+public class B {
+    public void method()
+    {
+        System.out.println("B");
+    }
+}
---
+++ src/main/java/com/jin/jdk8/defaultmethod/C.java
@@ -0,0 +1,13 @@
+package com.jin.jdk8.defaultmethod;
+
+/**
+ * @author wu.jinqing
+ * @date 2018年01月24日
+ */
+public class C extends B implements A {
+
+    public static void main(String[] args) {
+        C c = new C();
+        c.method();
+    }
+}
